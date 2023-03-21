7d8a80c85e7ccf7e99cdb219cd3e473b016fb6fd
ArIonMark1 <andreydatsenko777@gmail.com>
2023-03-21 22:28:38 +0100

    completed tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
 
+const elemButton = document.querySelector('.login-form');
+
+elemButton.addEventListener('submit', (event) => { 
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    alert(`Всі поля "${email.name}" та "${password.name}" повинні бути заповнені`);
+  }
+  
+  const inputData = {
+    email: email.value,
+    password: password.value,
+  }
+
+  console.log(inputData);
+  elemButton.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyPage = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+bodyPage.addEventListener('click', (event) => {
+
+  if (event.target.tagName === 'BUTTON') {
+    const currentColor = getRandomHexColor();
+
+    event.currentTarget.style.backgroundColor = currentColor;
+    spanEl.textContent = currentColor
+  }
+  console.log(bodyPage)
+
+});
